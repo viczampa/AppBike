cc3ddb496d9429de589412f797fcc763cf2f32fd
Henrique <henry_tuori@hotmail.com>
2016-10-12 01:26:33 -0300

    Atl config.xml com source npm, e mais plugin cordova de push

--- src/www/custom/global.js
+++ src/www/custom/global.js
@@ -15,6 +15,46 @@
 			firebase.initializeApp(config);
 		}, 5);
 	});
+	
+	if(PushNotification in window)
+	{
+		var push = PushNotification.init(
+		{
+			android:
+			{
+				senderID: "172245467834"
+			},
+			browser:
+			{
+				// pushServiceURL: 'http://push.api.phonegap.com/v1/push'
+			},
+			ios:
+			{
+				alert: "true",
+				badge: "true",
+				sound: "true"
+			},
+			windows: {}
+		});
+		push.on('registration', function(data)
+		{
+			// data.registrationId
+		});
+		push.on('notification', function(data)
+		{
+			// data.message,
+			// data.title,
+			// data.count,
+			// data.sound,
+			// data.image,
+			// data.additionalData
+		});
+
+		push.on('error', function(e)
+		{
+			// e.message
+		});
+	}
 
 	$(document).ready(function()
 	{
