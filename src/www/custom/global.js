bf5cbc18d88a3e34a5c4ee52013b1b0666f2e147
Henrique <henry_tuori@hotmail.com>
2016-10-12 01:36:35 -0300

    global.js plugin push cordova

--- src/www/custom/global.js
+++ src/www/custom/global.js
@@ -16,45 +16,48 @@
 		}, 5);
 	});
 	
-	if(PushNotification in window)
+	document.addEventListener("deviceready", function()
 	{
-		var push = PushNotification.init(
+		if(PushNotification in window)
 		{
-			android:
+			var push = PushNotification.init(
 			{
-				senderID: "172245467834"
-			},
-			browser:
+				android:
+				{
+					senderID: "172245467834"
+				},
+				browser:
+				{
+					// pushServiceURL: 'http://push.api.phonegap.com/v1/push'
+				},
+				ios:
+				{
+					alert: "true",
+					badge: "true",
+					sound: "true"
+				},
+				windows: {}
+			});
+			push.on('registration', function(data)
 			{
-				// pushServiceURL: 'http://push.api.phonegap.com/v1/push'
-			},
-			ios:
+				// data.registrationId
+				
+			});
+			push.on('notification', function(data)
 			{
-				alert: "true",
-				badge: "true",
-				sound: "true"
-			},
-			windows: {}
-		});
-		push.on('registration', function(data)
-		{
-			// data.registrationId
-		});
-		push.on('notification', function(data)
-		{
-			// data.message,
-			// data.title,
-			// data.count,
-			// data.sound,
-			// data.image,
-			// data.additionalData
-		});
-
-		push.on('error', function(e)
-		{
-			// e.message
-		});
-	}
+				// data.message,
+				// data.title,
+				// data.count,
+				// data.sound,
+				// data.image,
+				// data.additionalData
+			});
+			push.on('error', function(e)
+			{
+				// e.message
+			});
+		}
+	});
 
 	$(document).ready(function()
 	{
