a8863d38b0be2a196a67e2c2ce42ec1409b89f47
Henrique <henry_tuori@hotmail.com>
2016-10-16 19:08:25 -0200

    Global alterada com novas funções, eventos agora usam o jQuery, página de emparelhamentos terminada, alguns bugs relacionados a transição foram consertados

--- src/www/custom/main-emparelhados.js
+++ src/www/custom/main-emparelhados.js
@@ -1,9 +1,12 @@
+// Limpa as listas
+$('#listaRastreadores, #listaRastreados').children().remove();
+
 $.ajax(
 	{
 		url: basePepUrl + "lista_pareamentos.php",
 		success: function OnAjaxSuccess(data, textStatus, jqXHR)
 		{
-			console.log(typeof data, textStatus, jqXHR);
+			// console.log(typeof data, textStatus, jqXHR);
 			if(data.result === true)
 			{
 				$.each(data.data,function(index,obj)
@@ -12,50 +15,66 @@ $.ajax(
 					{
 						if(obj.aceito == false)
 						{
-							$('#listaRastreadores').append("<li class='table-view-cell' data-id='"+ obj.id +"'>"+
-																"<span class='email'>" + obj.email + "</span>" +
-																"<div>" +
-																	"<button class='btn btn-positive emp-aceitar'>Aceitar</button>" +
-																	"<button class='btn btn-negative emp-recusar'>Recusar</button>" +
-																"</div>" +
-															"</li>");
+							$('#listaRastreadores').append(
+							"<li class='table-view-cell dyna' data-id='"+ obj.id +"'>"+
+								"<span class='email'>" + obj.email + "</span>" +
+								"<div class='dyna'>" +
+									"<button class='btn btn-positive emp-aceitar'>Aceitar</button>" +
+									"<button class='btn btn-negative emp-recusar'>Recusar</button>" +
+								"</div>" +
+							"</li>");
 						}
 						else
 						{
-							$('#listaRastreados').append(Constr_Li_Aceito(obj.id, obj.email, false));
+							$('#listaRastreadores').append(Constr_Li_Aceito(obj.id, obj.email, obj.rastreado, obj.habilitado));
 						}
 					}
 					else
 					{
-						$('#listaRastreadores').append(Constr_Li_Aceito(obj.id, obj.email, true));
+						if(obj.aceito == false)
+						{
+							$('#listaRastreados').append(
+							"<li class='table-view-cell dyna' data-id='"+ obj.id +"'>"+
+								"<span class='email'>" + obj.email + "</span>" +
+								"<div>" +
+									"<span class='amz-pendente'>Pedido Pendente</span>" +
+								"</div>" +
+							"</li>");
+						}
+						else
+						{
+							$('#listaRastreados').append(Constr_Li_Aceito(obj.id, obj.email, obj.rastreado, obj.habilitado));
+						}
 					}
 				});
 			}
 			else
 			{
-
+				console.log(data, textStatus, jqXHR);
+				alert(data.message);
 			}
 		},
 		error: function OnAjaxError(jqXHR, textStatus, errorThrown)
 		{
+			alert('Erro ao carregar lista de pareados!');
 			console.log(jqXHR, textStatus, errorThrown);
 		},
 		complete: function OnAjaxComplete(jqXHR, textStatus)
 		{
-			console.log(jqXHR, textStatus);
+			// console.log(jqXHR, textStatus);
 		}
 });
 
-function Constr_Li_Aceito(id, email, rastreado)
+function Constr_Li_Aceito(id, email, rastreado, habilitado)
 {
-	var str = 	"<li class='table-view-cell' data-id='"+ id +"'>"+
+	var str = 	"<li class='table-view-cell dyna' data-id='"+ id +"'>"+
 					"<span class='email'>" + email + "</span>" +
-					"<div>" +
+					"<div class='dyna'>" +
 						"<button class='btn btn-negative emp-remover'>Remover</button>" +
 						(
-							(rastreado === false) ?
+							(rastreado == true) ?
 							("<button class='btn btn-positive emp-rastrear'>Rastrear</button>") :
-							("<div class='toggle active toggleHabilitado'>" +
+							("<div class='toggle" + ((habilitado == true) ? " active" : "") + " toggle-rastreamento'>" +
 								"<div class='toggle-handle'></div>" +
 							"</div>")
 						) +
@@ -66,6 +85,44 @@ function Constr_Li_Aceito(id, email, rastreado)
 
 $(document).ready(function()
 {
+	$('#listaRastreadores').on('toggle', '.toggle-rastreamento', function(event)
+	{
+		var detail = event.originalEvent.detail;
+		var id = $(event.target).closest('li').data('id');
+		$.ajax(
+		{
+			url: basePepUrl + "toggle_pareamento.php",
+			data:
+			{
+				id: id,
+				state: detail.isActive
+			},
+			success: function OnAjaxSuccess(data, textStatus, jqXHR)
+			{
+				// console.log(data, textStatus, jqXHR);
+				if(data.result === true)
+				{
+					alert(data.message);
+				}
+				else
+				{
+					alert(data.message);
+					TogglarBotaoRatchet(event.target);
+				}
+			},
+			error: function OnAjaxError(jqXHR, textStatus, errorThrown)
+			{
+				alert('Erro!');
+				console.log(jqXHR, textStatus, errorThrown);
+				TogglarBotaoRatchet(event.target);
+			},
+			complete: function OnAjaxComplete(jqXHR, textStatus)
+			{
+				// console.log(jqXHR, textStatus);
+			}
+		});
+	});
+	
 	$('#listaRastreadores').on('click','.emp-aceitar',function(event)
 	{
 		aceitaRecusa($(this).closest('li'), true);
@@ -78,7 +135,7 @@ $(document).ready(function()
 	$('#listaRastreados').on('click','.emp-rastrear',function(event)
 	{
 		window.ID_RASTREIO = $(this).closest('li').data('id');
-		PUSH({url: 'main-mapas.html', transition: 'slide-out'});
+		PUSHMASK({url: 'main-mapas.html', transition: 'slide-out'});
 	});
 
 	function aceitaRecusa(li, resp)
@@ -87,7 +144,6 @@ $(document).ready(function()
 		var email = li.find('.email').text();
 		var had = !!(li.find('.emp-remover').length === 1); // had = remover existente?
 		var rastreado = !!(li.closest('#listaRastreados').length === 1); // o alvo é um rastreado?
-		console.log('ID ' + id + " rastreado: ", rastreado);
 		$.ajax(
 		{
 			url: basePepUrl + "resp_pareamento.php",
@@ -100,17 +156,17 @@ $(document).ready(function()
 			},
 			success: function OnAjaxSuccess(data, textStatus, jqXHR)
 			{
-				console.log(data, textStatus, jqXHR);
+				// console.log(data, textStatus, jqXHR);
 				if(data.result === true)
 				{
-					alert(data.message);
+					// alert(data.message);
 					if(resp === false)
 					{
-						li.remove();
+						li.prettyDel();
 					}
 					else
 					{
-						li.replaceWith(Constr_Li_Aceito(id, email));
+						li.replaceWith(Constr_Li_Aceito(id, email, rastreado , false));
 					}
 				}
 				else
@@ -120,13 +176,13 @@ $(document).ready(function()
 			},
 			error: function OnAjaxError(jqXHR, textStatus, errorThrown)
 			{
+				alert('Erro!');
 				console.log(jqXHR, textStatus, errorThrown);
 			},
 			complete: function OnAjaxComplete(jqXHR, textStatus)
 			{
-				console.log(jqXHR, textStatus);
+				// console.log(jqXHR, textStatus);
 			}
 		});
 	}
-
 });
