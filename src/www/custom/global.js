b79f6a5acc4d09e95e7780640bcb9abd32dbdce8
Henrique <henry_tuori@hotmail.com>
2016-10-20 23:14:02 -0200

    Js fixes, incluído o Crosswalk no projeto para rodar em qualquer celular de forma consistente

--- src/www/custom/global.js
+++ src/www/custom/global.js
@@ -1,343 +1,346 @@
 (function()
 {
-	window.basePepUrl = 'http://pepperdrinks.smserver.com.br/app/src/public_html/';
-	// window.basePepUrl = 'http://localhost:80/AppBikeServer/src/public_html/';
-	window.artificialHistory = [];
-	window.INTERVAL_CLEANUP = function(){};
-	window.custom_back_key = function(){};
-	window.navFromBack = false;
+	$(document).on('deviceready', function()
+	{
+		window.basePepUrl = 'http://pepperdrinks.smserver.com.br/app/src/public_html/';
+		// window.basePepUrl = 'http://localhost:80/AppBikeServer/src/public_html/';
+		window.artificialHistory = [];
+		window.INTERVAL_CLEANUP = function(){};
+		window.custom_back_key = function(){};
+		window.navFromBack = false;
 
-	$.ajaxSetup({
-		cache: false,
-		dataType: "json",
-		method:"POST",
-		jsonp: false,
-		type: "POST",
-		async: true,
-		timeout: 10000,
-		// CORS transmissao de Cookies
-		xhrFields:
+		$.ajaxSetup(
 		{
-			withCredentials: true
-		}
-	});
-
-	window.getDistanceFromLatLonInKm = function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2)
-	{
-		// Filter
-		if(typeof lat1 === 'undefined' || typeof lon1 === 'undefined' || typeof lat2 === 'undefined' || typeof lon2 === 'undefined')
-		{
-			return Infinity;
-		}
-
-		var R = 6371; // Radius of the earth in km
-		var dLat = deg2rad(lat2-lat1);  // deg2rad below
-		var dLon = deg2rad(lon2-lon1);
-
-		var a =
-		Math.sin(dLat/2) * Math.sin(dLat/2) +
-		Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
-		Math.sin(dLon/2) * Math.sin(dLon/2)
-		;
-
-		var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-		var d = R * c; // Distance in km
-
-		return d;
-	}
-
-	function deg2rad(deg)
-	{
-		return deg * (Math.PI/180);
-	}
-
-	window.getDistanceFromLatLonInM = function getDistanceFromLatLonInM(lat1, lon1, lat2, lon2)
-	{
-		return getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2) * 1000.0;
-	}
-
-	window.IniciarTransmissao = function IniciarTransmissao()
-	{
-		$.getScript('custom/transmitir.js', function(data, textStatus, jqxhr)
-		{
-			// Success, pegou o script e está rodando/rodou
-		});
-	};
-
-	document.addEventListener("deviceready", function(event)
-	{
-		// alert("deviceready");
-		if(typeof PushNotification !== 'undefined')
-		{
-			// alert("Push disponível!")
-			$(window).one('appb_login', function()
+			cache: false,
+			dataType: "json",
+			method:"POST",
+			jsonp: false,
+			type: "POST",
+			async: true,
+			timeout: 10000,
+			// CORS transmissao de Cookies
+			xhrFields:
 			{
-				// alert("Login feito, bindando push!");
-				var push = PushNotification.init(
-				{
-					android:
-					{
-						senderID: "172245467834"
-					},
-					browser:
-					{
-						// pushServiceURL: 'http://push.api.phonegap.com/v1/push'
-					},
-					ios:
-					{
-						alert: "true",
-						badge: "true",
-						sound: "true"
-					},
-					windows: {}
-				});
-				push.on('registration', function(data)
-				{
-					// data.registrationId
-					alert("Push registrado! \n\n OBJ: " + JSON.stringify(data));
-					$.ajax(
-					{
-						url: basePepUrl + "push_reg.php",
-						data:
-						{
-							regid: data.registrationId
-						},
-						success: function OnAjaxSuccess(data, textStatus, jqXHR)
-						{
-							console.log(data, textStatus, jqXHR);
-							if(data.result === true)
-							{
-
-							}
-							else
-							{
-
-							}
-						},
-						error: function OnAjaxError(jqXHR, textStatus, errorThrown)
-						{
-							console.log(jqXHR, textStatus, errorThrown);
-						},
-						complete: function OnAjaxComplete(jqXHR, textStatus)
-						{
-							// console.log(jqXHR, textStatus);
-						}
-					});
-				});
-				push.on('notification', function(data)
-				{
-					// data.message,
-					// data.title,
-					// data.count,
-					// data.sound,
-					// data.image,
-					// data.additionalData
-					alert("Push recebido! \n\n OBJ: " + JSON.stringify(data));
-				});
-				push.on('error', function(e)
-				{
-					// e.message
-					alert("Push error! \n\n OBJ: " + JSON.stringify(e));
-				});
-			});
-		}
-		else
-		{
-			alert('Push indisponivel');
-		}
-	});
-
-
-	/*
-	$.getScript( "https://www.gstatic.com/firebasejs/3.4.1/firebase.js", function(data, textStatus, jqxhr)
-	{
-		window.setTimeout(function()
-		{
-			var config =
-			{
-				apiKey: "AIzaSyCyLB3C18FS_FSLkpoYKGp1Hig-vytaumg",
-				authDomain: "appbike-146203.firebaseapp.com",
-				databaseURL: "https://appbike-146203.firebaseio.com",
-				storageBucket: "appbike-146203.appspot.com",
-				messagingSenderId: "172245467834"
-			};
-			firebase.initializeApp(config);
-		}, 5);
-	});
-	*/
-
-
-	(function($)
-	{
-		var maskstring = '<div id="mask"><div id="loader"></div></div>';
-		function ChkMaskAppend()
-		{
-			if($('#mask').length === 0)
-			{
-				$('body').append(maskstring);
+				withCredentials: true
 			}
-		}
-		window.showMask = function showMask()
-		{
-			ChkMaskAppend();
-			$('#mask').addClass('show');
-		}
-		window.hideMask = function hideMask()
-		{
-			$('#mask').removeClass('show');
-		}
-		window.toggleMask = function toggleMask()
-		{
-			ChkMaskAppend();
-			$('#mask').toggleClass('show');
-		}
-	})(jQuery);
-
-
-
-	$(document).ready(function()
-	{
-		$(document).on('click', '.logout-onclick', function(event)
-		{
-			showMask();
-			$.ajax(
-			{
-				url: basePepUrl + "logout.php",
-				data:
-				{
-
-				},
-				success: function OnAjaxSuccess(data, textStatus, jqXHR)
-				{
-					if(data.result === true)
-					{
-						// alert(data.message);
-						localStorage.removeItem('login_pwd');
-						PUSHMASK({url: 'index.html', transition: 'slide-out'});
-					}
-					else
-					{
-						alert(data.message);
-					}
-				},
-				error: function OnAjaxError(jqXHR, textStatus, errorThrown)
-				{
-					console.log(jqXHR, textStatus, errorThrown);
-				},
-				complete: function OnAjaxComplete(jqXHR, textStatus)
-				{
-					hideMask();
-				}
-			});
 		});
-	});
 
-
-	window.TogglarBotaoRatchet = function TogglarBotaoRatchet(botao)
-	{
-		var ativo = $(botao).hasClass('active');
-		if(ativo)
+		window.getDistanceFromLatLonInKm = function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2)
 		{
-			$(botao).removeClass('active');
-			$(botao).children().css('transform', '');
+			// Filter
+			if(typeof lat1 === 'undefined' || typeof lon1 === 'undefined' || typeof lat2 === 'undefined' || typeof lon2 === 'undefined')
+			{
+				return Infinity;
+			}
+
+			var R = 6371; // Radius of the earth in km
+			var dLat = deg2rad(lat2-lat1);  // deg2rad below
+			var dLon = deg2rad(lon2-lon1);
+
+			var a =
+			Math.sin(dLat/2) * Math.sin(dLat/2) +
+			Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+			Math.sin(dLon/2) * Math.sin(dLon/2)
+			;
+
+			var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+			var d = R * c; // Distance in km
+
+			return d;
 		}
-		else
+
+		function deg2rad(deg)
 		{
-			$(botao).addClass('active');
+			return deg * (Math.PI/180);
 		}
-	}
 
-
-
-
-	window.smallTimeout = function smallTimeout(func)
-	{
-		window.setTimeout(func, 10);
-	}
-
-
-	window.PUSHMASK = function PUSHMASK(push_opt)
-	{
-		showMask();
-		$(window).one('push', function(event){ hideMask(); });
-		PUSH(push_opt);
-	}
-
-
-
-	window.instantTimeout = function instantTimeout(func)
-	{
-		window.setTimeout(func, 0);
-	}
-
-
-
-	$.fn.prettyDel = function prettyDel(color, time)
-	{
-		color = color || 'red';
-		time = time || 400;
-		var item = $(this);
-		item.css('background-color', color);
-		item.css('overflow', 'hidden');
-		item.css('opacity', '0.75');
-		item.css('position', 'relative');
-		item.animate({ 'background-color': 'transparent', 'padding-top': '0', 'padding-bottom': '0', 'opacity': '0', 'height': '0'/*, 'top': '-' + parseFloat(item.innerHeight()) + "px"*/ }, { duration: time, queue: false, always: function(){ item.remove(); } } );
-	}
-
-
-
-
-	document.addEventListener("backbutton", onBackKeyDown, false);
-	function onBackKeyDown(event)
-	{
-		var gh = window.custom_back_key();
-		if(gh !== true)
+		window.getDistanceFromLatLonInM = function getDistanceFromLatLonInM(lat1, lon1, lat2, lon2)
 		{
-			history.back();
+			return getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2) * 1000.0;
 		}
-	}
 
-
-	window.addEventListener('popstate', function(event)
-	{
-		// console.log(event);
-		// Window.History.Back
-		// if(!event.state)
+		window.IniciarTransmissao = function IniciarTransmissao()
 		{
-			window.artificialHistory.pop();
-			PUSHMASK({url: window.artificialHistory.pop(), transition: 'slide-out'});
-			window.navFromBack = true;
-		}
-	});
-
-
-
-	// Evento de navegação pela PUSH
-	function checkPage()
-	{
-		// Histórico artificial
-		window.artificialHistory.push(window.location.href);
-
-		// Se estiver transmitindo algo ou recebendo algo, limpar essas porras AGORA
-		window.INTERVAL_CLEANUP();
-
-		// Achar os js da página que foi carregada aqui, e rodá-los
-		var scripts = $('.content').find('script');
-		scripts.each(function(index, script)
-		{
-			script = $(script);
-			var orig_src = script.attr('src');
-			$.getScript( orig_src, function(data, textStatus, jqxhr)
+			$.getScript('custom/transmitir.js', function(data, textStatus, jqxhr)
 			{
 				// Success, pegou o script e está rodando/rodou
 			});
+		};
+
+		document.addEventListener("deviceready", function(event)
+		{
+			// alert("deviceready");
+			if(typeof PushNotification !== 'undefined')
+			{
+				// alert("Push disponível!")
+				$(window).one('appb_login', function()
+				{
+					// alert("Login feito, bindando push!");
+					var push = PushNotification.init(
+					{
+						android:
+						{
+							senderID: "172245467834"
+						},
+						browser:
+						{
+							// pushServiceURL: 'http://push.api.phonegap.com/v1/push'
+						},
+						ios:
+						{
+							alert: "true",
+							badge: "true",
+							sound: "true"
+						},
+						windows: {}
+					});
+					push.on('registration', function(data)
+					{
+						// data.registrationId
+						alert("Push registrado! \n\n OBJ: " + JSON.stringify(data));
+						$.ajax(
+						{
+							url: basePepUrl + "push_reg.php",
+							data:
+							{
+								regid: data.registrationId
+							},
+							success: function OnAjaxSuccess(data, textStatus, jqXHR)
+							{
+								console.log(data, textStatus, jqXHR);
+								if(data.result === true)
+								{
+
+								}
+								else
+								{
+
+								}
+							},
+							error: function OnAjaxError(jqXHR, textStatus, errorThrown)
+							{
+								console.log(jqXHR, textStatus, errorThrown);
+							},
+							complete: function OnAjaxComplete(jqXHR, textStatus)
+							{
+								// console.log(jqXHR, textStatus);
+							}
+						});
+					});
+					push.on('notification', function(data)
+					{
+						// data.message,
+						// data.title,
+						// data.count,
+						// data.sound,
+						// data.image,
+						// data.additionalData
+						alert("Push recebido! \n\n OBJ: " + JSON.stringify(data));
+					});
+					push.on('error', function(e)
+					{
+						// e.message
+						alert("Push error! \n\n OBJ: " + JSON.stringify(e));
+					});
+				});
+			}
+			else
+			{
+				alert('Push indisponivel');
+			}
 		});
 
-		// Attach novamente
-		$(window).one('push', checkPage);
-	}
 
-	// Manualmente chamar evento PUSH inicial
-	checkPage();
+		/*
+		$.getScript( "https://www.gstatic.com/firebasejs/3.4.1/firebase.js", function(data, textStatus, jqxhr)
+		{
+			window.setTimeout(function()
+			{
+				var config =
+				{
+					apiKey: "AIzaSyCyLB3C18FS_FSLkpoYKGp1Hig-vytaumg",
+					authDomain: "appbike-146203.firebaseapp.com",
+					databaseURL: "https://appbike-146203.firebaseio.com",
+					storageBucket: "appbike-146203.appspot.com",
+					messagingSenderId: "172245467834"
+				};
+				firebase.initializeApp(config);
+			}, 5);
+		});
+		*/
 
+
+		(function($)
+		{
+			var maskstring = '<div id="mask"><div id="loader"></div></div>';
+			function ChkMaskAppend()
+			{
+				if($('#mask').length === 0)
+				{
+					$('body').append(maskstring);
+				}
+			}
+			window.showMask = function showMask()
+			{
+				ChkMaskAppend();
+				$('#mask').addClass('show');
+			}
+			window.hideMask = function hideMask()
+			{
+				$('#mask').removeClass('show');
+			}
+			window.toggleMask = function toggleMask()
+			{
+				ChkMaskAppend();
+				$('#mask').toggleClass('show');
+			}
+		})(jQuery);
+
+
+
+		$(document).ready(function()
+		{
+			$(document).on('click', '.logout-onclick', function(event)
+			{
+				showMask();
+				$.ajax(
+				{
+					url: basePepUrl + "logout.php",
+					data:
+					{
+
+					},
+					success: function OnAjaxSuccess(data, textStatus, jqXHR)
+					{
+						if(data.result === true)
+						{
+							// alert(data.message);
+							localStorage.removeItem('login_pwd');
+							PUSHMASK({url: 'index.html', transition: 'slide-out'});
+						}
+						else
+						{
+							alert(data.message);
+						}
+					},
+					error: function OnAjaxError(jqXHR, textStatus, errorThrown)
+					{
+						console.log(jqXHR, textStatus, errorThrown);
+					},
+					complete: function OnAjaxComplete(jqXHR, textStatus)
+					{
+						hideMask();
+					}
+				});
+			});
+		});
+
+
+		window.TogglarBotaoRatchet = function TogglarBotaoRatchet(botao)
+		{
+			var ativo = $(botao).hasClass('active');
+			if(ativo)
+			{
+				$(botao).removeClass('active');
+				$(botao).children().css('transform', '');
+			}
+			else
+			{
+				$(botao).addClass('active');
+			}
+		}
+
+
+
+
+		window.smallTimeout = function smallTimeout(func)
+		{
+			window.setTimeout(func, 10);
+		}
+
+
+		window.PUSHMASK = function PUSHMASK(push_opt)
+		{
+			showMask();
+			$(window).one('push', function(event){ hideMask(); });
+			PUSH(push_opt);
+		}
+
+
+
+		window.instantTimeout = function instantTimeout(func)
+		{
+			window.setTimeout(func, 0);
+		}
+
+
+
+		$.fn.prettyDel = function prettyDel(color, time)
+		{
+			color = color || 'red';
+			time = time || 400;
+			var item = $(this);
+			item.css('background-color', color);
+			item.css('overflow', 'hidden');
+			item.css('opacity', '0.75');
+			item.css('position', 'relative');
+			item.animate({ 'background-color': 'transparent', 'padding-top': '0', 'padding-bottom': '0', 'opacity': '0', 'height': '0'/*, 'top': '-' + parseFloat(item.innerHeight()) + "px"*/ }, { duration: time, queue: false, always: function(){ item.remove(); } } );
+		}
+
+
+
+
+		document.addEventListener("backbutton", onBackKeyDown, false);
+		function onBackKeyDown(event)
+		{
+			var gh = window.custom_back_key();
+			if(gh !== true)
+			{
+				history.back();
+			}
+		}
+
+
+		window.addEventListener('popstate', function(event)
+		{
+			// console.log(event);
+			// Window.History.Back
+			// if(!event.state)
+			{
+				window.artificialHistory.pop();
+				PUSHMASK({url: window.artificialHistory.pop(), transition: 'slide-out'});
+				window.navFromBack = true;
+			}
+		});
+
+
+
+		// Evento de navegação pela PUSH
+		function checkPage()
+		{
+			// Histórico artificial
+			window.artificialHistory.push(window.location.href);
+
+			// Se estiver transmitindo algo ou recebendo algo, limpar essas porras AGORA
+			window.INTERVAL_CLEANUP();
+
+			// Achar os js da página que foi carregada aqui, e rodá-los
+			var scripts = $('.content').find('script');
+			scripts.each(function(index, script)
+			{
+				script = $(script);
+				var orig_src = script.attr('src');
+				$.getScript( orig_src, function(data, textStatus, jqxhr)
+				{
+					// Success, pegou o script e está rodando/rodou
+				});
+			});
+
+			// Attach novamente
+			$(window).one('push', checkPage);
+		}
+
+		// Manualmente chamar evento PUSH inicial
+		checkPage();
+	});
 })();
--- src/www/custom/index.js
+++ src/www/custom/index.js
@@ -1,108 +1,111 @@
 $(document).ready(function()
 {
-	var user_i = $('#user');
-	var senha_i = $('#senha');
-	var lembra_login = $('#lembrarLogin');
-
-	$('#cadastrarBtn').on('click', function(event)
+	$(document).on('deviceready', function()
 	{
-		showMask();
-		$.ajax(
-		{
-			url: basePepUrl + "cadastro.php",
-			data:
-			{
-				email : $('#cad_email').val(),
-				senha : $('#cad_senha').val(),
-				nome : $('#cad_nome').val()
-			},
-			success: function OnAjaxSuccess(data, textStatus, jqXHR)
-			{
-				if(data.result === true)
-				{
-					alert(data.message);
-				}
-				else
-				{
-					alert(data.message);
-				}
-			},
-			error: function OnAjaxError(jqXHR, textStatus, errorThrown)
-			{
-				console.log(jqXHR, textStatus, errorThrown);
-			},
-			complete: function OnAjaxComplete(jqXHR, textStatus)
-			{
-				hideMask();
-			}
-		});
-	});
+		var user_i = $('#user');
+		var senha_i = $('#senha');
+		var lembra_login = $('#lembrarLogin');
 
-	$('#loginBtn').on('click', function(event)
-	{
-		showMask();
-		$.ajax(
+		$('#cadastrarBtn').on('click', function(event)
 		{
-			url: basePepUrl + "login.php",
-			data:
+			showMask();
+			$.ajax(
 			{
-				user : user_i.val(),
-				senha : senha_i.val()
-			},
-			success: function OnAjaxSuccess(data, textStatus, jqXHR)
-			{
-				if(data.result === true)
+				url: basePepUrl + "cadastro.php",
+				data:
 				{
-					if(lembra_login.hasClass('active') === true)
+					email : $('#cad_email').val(),
+					senha : $('#cad_senha').val(),
+					nome : $('#cad_nome').val()
+				},
+				success: function OnAjaxSuccess(data, textStatus, jqXHR)
+				{
+					if(data.result === true)
 					{
-						localStorage.setItem('login_usr', $('#user').val());
-						localStorage.setItem('login_pwd', $('#senha').val());
+						alert(data.message);
 					}
 					else
 					{
-						localStorage.removeItem('login_usr');
-						localStorage.removeItem('login_pwd');
+						alert(data.message);
 					}
-					// Evento login
-					$(window).trigger('appb_login');
-					// Push troca página pelo JS
-					PUSH({url: 'menu-principal.html', transition: 'slide-in'});
-				}
-				else
+				},
+				error: function OnAjaxError(jqXHR, textStatus, errorThrown)
 				{
-					alert(data.message);
+					console.log(jqXHR, textStatus, errorThrown);
+				},
+				complete: function OnAjaxComplete(jqXHR, textStatus)
+				{
+					hideMask();
 				}
-			},
-			error: function OnAjaxError(jqXHR, textStatus, errorThrown)
-			{
-				console.log(jqXHR, textStatus, errorThrown);
-			},
-			complete: function OnAjaxComplete(jqXHR, textStatus)
-			{
-				hideMask();
-			}
+			});
 		});
+
+		$('#loginBtn').on('click', function(event)
+		{
+			showMask();
+			$.ajax(
+			{
+				url: basePepUrl + "login.php",
+				data:
+				{
+					user : user_i.val(),
+					senha : senha_i.val()
+				},
+				success: function OnAjaxSuccess(data, textStatus, jqXHR)
+				{
+					if(data.result === true)
+					{
+						if(lembra_login.hasClass('active') === true)
+						{
+							localStorage.setItem('login_usr', $('#user').val());
+							localStorage.setItem('login_pwd', $('#senha').val());
+						}
+						else
+						{
+							localStorage.removeItem('login_usr');
+							localStorage.removeItem('login_pwd');
+						}
+						// Evento login
+						$(window).trigger('appb_login');
+						// Push troca página pelo JS
+						PUSH({url: 'menu-principal.html', transition: 'slide-in'});
+					}
+					else
+					{
+						alert(data.message);
+					}
+				},
+				error: function OnAjaxError(jqXHR, textStatus, errorThrown)
+				{
+					console.log(jqXHR, textStatus, errorThrown);
+				},
+				complete: function OnAjaxComplete(jqXHR, textStatus)
+				{
+					hideMask();
+				}
+			});
+		});
+
+		user_i.val(localStorage.getItem('login_usr') || '');
+		senha_i.val(localStorage.getItem('login_pwd') || '');
+
+		if(user_i.val().length > 0 && senha_i.val().length > 0 && window.navFromBack !== true)
+		{
+			// Faz login automático
+			$('#loginBtn').click();
+		}
+
+		window.navFromBack = false;
+
+		window.custom_back_key = function()
+		{
+			navigator.app.exitApp();
+			return false;
+		};
+
+		window.INTERVAL_CLEANUP = function()
+		{
+			window.custom_back_key = function(){};
+		}
 	});
-
-	user_i.val(localStorage.getItem('login_usr') || '');
-	senha_i.val(localStorage.getItem('login_pwd') || '');
-
-	if(user_i.val().length > 0 && senha_i.val().length > 0 && window.navFromBack !== true)
-	{
-		// Faz login automático
-		$('#loginBtn').click();
-	}
-
-	window.navFromBack = false;
-
-	window.custom_back_key = function()
-	{
-		navigator.app.exitApp();
-		return false;
-	};
-
-	window.INTERVAL_CLEANUP = function()
-	{
-		window.custom_back_key = function(){};
-	}
 });
